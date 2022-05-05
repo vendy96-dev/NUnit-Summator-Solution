d00dc834115a4624a25c7796d6887b3f7d3c0c86
vendy96-dev <58224013+vendy96-dev@users.noreply.github.com>
2022-05-05 20:42:18 +0300

    Added Folder

---
+++ Lecture-NUnit-Summator-Project/Summator-Project/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Summator_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Summator.Sum(new int[] { 1, 2, 3, 4, 5, 6 });
+            if(result == 20)
+            {
+                Console.WriteLine("TEST PASS");
+            } else
+            {
+                Console.WriteLine("TEST FAIL");
+            }
+        }
+    }
+}
---
+++ Lecture-NUnit-Summator-Project/Summator-Project/Summator.cs
@@ -0,0 +1,24 @@
+﻿namespace Summator_Project
+{
+    public static class Summator
+    {
+        public static int Sum(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+        public static int Average(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum/arr.Length;
+        }
+    }
+}
